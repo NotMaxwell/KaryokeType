f68b65ab9f909e2edd1b981ef0e91a74554bc536
NotMaxwell <william.max.mccormick@gmail.com>
2025-06-29 22:08:00 -0400

    fixed launching the site?

--- main.go
+++ main.go
@@ -10,14 +10,17 @@ func main() {
 	r := gin.Default()
 
 	r.Static("/static", "./static")
-	r.LoadHTMLGlob("templates/*")
+	r.LoadHTMLGlob("views/*")
 
 	r.GET("/", mainPage())
 
+	r.Run(":8080") // Explicitly specify port
 }
 
 func mainPage() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.HTML(http.StatusOK, "views/index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "KaryokeType",
+		})
 	}
 }
